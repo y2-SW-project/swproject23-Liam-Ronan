5a6d52c85b99a529cee2ac18997dea4dde9e9e29
Liam Ronan <liamronan16@gmail.com>
2023-02-15 18:44:29 +0000

    Created the laravel project

--- laravel9-Barbershop/config/mail.php
+++ laravel9-Barbershop/config/mail.php
@@ -51,16 +51,10 @@ return [
 
         'mailgun' => [
             'transport' => 'mailgun',
-            // 'client' => [
-            //     'timeout' => 5,
-            // ],
         ],
 
         'postmark' => [
             'transport' => 'postmark',
-            // 'client' => [
-            //     'timeout' => 5,
-            // ],
         ],
 
         'sendmail' => [
