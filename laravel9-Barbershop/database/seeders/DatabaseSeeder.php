b5a88d47bf17d7bd3fec16c774516825c736ab14
Liam Ronan <liamronan16@gmail.com>
2023-02-27 19:08:28 +0000

    added database scheme diagram to diagrams folder

--- laravel9-Barbershop/database/seeders/DatabaseSeeder.php
+++ laravel9-Barbershop/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Booking;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        Booking::factory(6)->create();
+
+        /*  \App\Models\User::factory(10)->create(); */
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
