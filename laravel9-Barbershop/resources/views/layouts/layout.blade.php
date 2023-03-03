c5d68754246f3a52b1b8f67b9ecc65fa2ebe056d
Liam Ronan <liamronan16@gmail.com>
2023-03-03 11:36:54 +0000

    worked on bootstrap and sass for the homepage, fonts, colours, gradients. Must finish footer and develop statis sections

--- laravel9-Barbershop/resources/views/layouts/layout.blade.php
+++ laravel9-Barbershop/resources/views/layouts/layout.blade.php
@@ -5,17 +5,14 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>Project</title>
-		<link rel="stylesheet" href="css/app.css" />
-		<link
-			rel="stylesheet"
-			href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css"
-		/>
+		<link rel="stylesheet" href="resources/css/app.css" />
+		@vite(['resources/js/app.js'])
 	</head>
 	<body>
 		<header>
 			<nav class="navbar fixed-top bg-blur navbar-expand-lg py-4">
 				<div class="container-xl">
-						<a class="heading text-primary navbar-brand fs-2 fw-bold" href="#">EasyEvents</a>
+						<a class="heading text-light navbar-brand fs-2 fw-bold" href="#">Boyz2Men</a>
 						<button
 							class="navbar-toggler bg-light"
 							type="button"
@@ -36,19 +33,19 @@
                                         <a class="nav-link text-light" href="../index.html">Home</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link text-light" href="../Pages/events.html">Events</a>
+                                        <a class="nav-link text-light" href="../Pages/events.html">Bookings</a>
                                     </li>
                                     <li class="nav-item">
                                         <a class="nav-link text-light" href="../Pages/aboutUs.html">About</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link text-light" href="#">Your tickets</a>
+                                        <a class="nav-link text-light" href="#">Contact</a>
                                     </li>
                                 </ul>
                             </div>
 				
                             
-							<button class="main-btn gradient btn btn-outline-primary rounded-pill fs-5 mt-2">
+							<button class="main-btn gradient btn fs-5 mt-2">
 								<a class="text-light text-center p-3 text-decoration-none fw-semibold" href="../Pages/register.html">Register</a>
 							</button>
 						</div>
@@ -56,6 +53,18 @@
 					</div>
 				</div>
 			</nav>
+			<div class="bg d-flex min-vh-100">
+				<div class="align-self-center text-center text-light col-md-8 offset-md-2">
+					<h1 class="heading display-1 fw-semi-bold pb-3 mt-5">Looking Your Best Never Goes Out of<strong class="other-colour text-danger fw-bold"> Style</strong><h1>
+								
+					<p class="body lead pb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin faucibus porttitor dui.Lorem ipsum dolor sit amet consectetur. Neque morbi sed mauris facilisi pulvinar nisl elit. Lacus elementum fringilla et habitant tellus a massa </p>
+			
+			
+					<button class="gradient btn fs-5 my-2">
+						<a class="text-light p-3 text-decoration-none fw-semibold" href="../Pages/events.html">Book Appointment</a>
+					</button>					
+				</div>
+			</div>
         </header>
 
         <main>
@@ -67,14 +76,8 @@
 				<div class="container">
 					<div class="row">
 						<div class="col-md-9">
-							<h4 class="heading text-decoration-none text-primary fw-normal display-4 py-4" href="#">EasyEvents</h4>
-						</div>
-						<div class="col-md-3 my-5">
-							<a class="social" href="#"><img class="img-fluid mx-2" src="social/FB.png" alt="Facebook"></a>
-							<a class="social" href="#"><img class="img-fluid mx-2" src="social/instagram.png" alt="instagram"></a>
-							<a class="social" href="#"><img class="img-fluid mx-2" src="social/linkedin.png" alt="LinkedIn"></a>
-							<a class="social" href="#"><img class="img-fluid mx-2" src="social/Twitter.png" alt="Twitter"></a>
-						</div>							
+							<h4 class="heading text-decoration-none text-light fw-normal display-4 py-4" href="#">Boyz2Men</h4>
+						</div>						
 					</div>
 				</div>
 
