76bbda048cf71abe7ce84b34132d5c0cdbe4fce1
Liam Ronan <liamronan16@gmail.com>
2023-04-05 10:47:58 +0100

    Fixed vite.config.js and links to scss for performace

--- laravel9-Barbershop/resources/views/layouts/layout.blade.php
+++ laravel9-Barbershop/resources/views/layouts/layout.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Boyz2Men</title>
-    <link rel="stylesheet" href="resources/css/app.css" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
     <script src="//unpkg.com/alpinejs" defer></script>
     @vite(['resources/js/app.js'])
 </head>
