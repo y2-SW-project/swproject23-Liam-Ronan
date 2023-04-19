4d532b7763c92ffc803cf916d05ee94eed8f7564
Liam Ronan <liamronan16@gmail.com>
2023-04-19 19:53:45 +0100

    added sequence diagram to diagrams folder

--- laravel9-Barbershop/tests/Feature/BookingTest.php
+++ laravel9-Barbershop/tests/Feature/BookingTest.php
@@ -2,11 +2,14 @@
 
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\Barber;
 use App\Models\Booking;
+use App\Models\Services;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class BookingTest extends TestCase
 {
-    
+    use RefreshDatabase;
+
 }
